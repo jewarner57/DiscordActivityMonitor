876ec234210378bcf77f66dad423292089e014d9
Jonathan Warner <jewarner57@gmail.com>
2022-03-11 21:10:06 -0500

    format fixes

--- src/models/activity.js
+++ src/models/activity.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ActivitySchema = new Schema({
-    guildID: { type: String, required: true },
-    channelID: { type: String, required: true },
-    userIDs: { type: [String], required: true },
+  guildID: { type: String, required: true },
+  channelID: { type: String, required: true },
+  userIDs: { type: [String], required: true },
 }, { timestamps: { createdAt: 'created_at' } });
 
 module.exports = mongoose.model('Activity', ActivitySchema)
